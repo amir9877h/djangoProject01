a286b9761789e9411c83074830ab57bce8d5ee18
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2024-01-08 21:58:35 +0330

    Create Catalogue Model And Migrate it

--- djangoProject01/settings.py
+++ djangoProject01/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = "djangoProject01.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": DB["NAME"],
+        "USER": DB["USER"],
+        "PASSWORD": DB["PASSWORD"],
+        "HOST": DB["HOST"],
+        "PORT": DB["PORT"],
     }
 }
 
