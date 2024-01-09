a80eb1ae88b947ccb3f25aa6640592381d6100f8
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2024-01-09 21:15:46 +0330

    How To Get Users Balance with aggregate and annotate...

--- transaction/models.py
+++ transaction/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Count, Sum, Q
+from django.db.models.functions import Coalesce
 
 
 # Create your models here.
@@ -20,3 +22,29 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f'{self.user}: {self.get_transaction_type_display()} -> {self.amount} Rial'
+
+    @classmethod
+    def get_report(cls):
+        total_charges = Sum('transactions__amount', filter=Q(transactions__transaction_type=cls.CHARGE))
+        total_purchases = Sum('transactions__amount', filter=Q(transactions__transaction_type=cls.PURCHASE))
+        total_transfers = Sum('transactions__amount', filter=Q(transactions__transaction_type=cls.TRANSFER))
+
+        users = User.objects.all().annotate(
+            transactions_count=Count('transactions__id'),
+            balance=Coalesce(total_charges, 0) - Coalesce(total_purchases, 0) + Coalesce(total_transfers, 0)
+        )
+        return users
+
+    @classmethod
+    def get_total_balance(cls, user):
+        querySet = cls.get_report()
+        return querySet.aggregate(Sum('balance'))
+
+
+class UserBalance(models.Model):
+    user = models.ForeignKey(User, related_name='balance_records', on_delete=models.RESTRICT)
+    balance = models.BigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}: {self.balance} - {self.created_at}'
